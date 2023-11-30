84529a72f3efb414735fe918e7ca8228ad6c69bc
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-11-30 20:55:18 +0100

    about section

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
+import ActiveSectionContextProvider from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -55,9 +56,11 @@ export default function RootLayout({
           2xl:left-[-5rem]
         '>
         </div>
+          <ActiveSectionContextProvider>
             <Header />
             {/* page is the children of layout */}
             {children}
+          </ActiveSectionContextProvider>
         </body>
     </html>
   )
---
+++ components/about.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>A propos</SectionHeading>
+      <p className="mb-3">
+      Ayant récemment obtenu mon{" "}
+        <span className="font-medium">Certificat Fédéral de Capacité</span> en juillet 2023, 
+        j&apos;ai décidé de suivre ma <span className="underline">passion</span> pour la programmation.
+        Actuellement, je me prépare à intégrer une{" "}
+        <span className="font-medium">école supérieure en développement d&apos;applications en 2024</span>.{" "}
+        En ce moment, Je me concentre sur l&apos;apprentissage de{" "} <span className="font-medium"> React, Next.js & Django</span>
+        . Je également familier avec le framework <span className="underline">Symfony</span> avec lequel j&apos;ai réalisé mon Travail pratique individuel 
+        <span className="font-medium">(TPI)</span> où j&apos;ai obtenu la note de <span className="font-medium">5,6</span>
+        . 
+      </p>
+
+      <p>
+        Mon objectif est d&apos;approfondir mes <span className="font-medium">compétences en développement</span> et de 
+        contribuer à des projets innovants.
+        {" "}
+        je suis <span className="font-medium underline">motivé</span> et <span className="font-medium underline">créatif</span>
+         , prêt à relever de nouveaux défis dans le monde du développement d&apos;applications.
+      </p>
+    </motion.section>
+  );
+}
