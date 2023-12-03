064a483967939667b33fa6356b5b55049e2e3239
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-12-03 09:27:28 +0100

    fix issues

--- components/skills.tsx
+++ components/skills.tsx
@@ -21,7 +21,7 @@ const fadeInAnimationVariants = {
 };
 
 export default function Skills() {
-  const { ref } = useSectionInView("Skills");
+  const { ref } = useSectionInView("Compétences");
 
   return (
     <section
